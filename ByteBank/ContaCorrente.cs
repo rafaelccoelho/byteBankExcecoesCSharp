7f80c88cb2c93a10a85964f59a7d03f79a64e23c
Rafael <cezariocoelho@gmail.com>
2021-08-23 20:58:20 -0300

    ReadOnly - alterar campo somente no construtor, campo agencia e numero

--- ByteBank/ContaCorrente.cs
+++ ByteBank/ContaCorrente.cs
@@ -11,25 +11,8 @@ namespace ByteBank
 
         public static int TotalDeContasCriadas { get; private set; }
 
-
-        private int _agencia;
-        public int Agencia
-        {
-            get
-            {
-                return _agencia;
-            }
-            set
-            {
-                if (value <= 0)
-                {
-                    return;
-                }
-
-                _agencia = value;
-            }
-        }
-        public int Numero { get; set; }
+        public int Agencia{get;}
+        public int Numero { get;}
 
         private double _saldo = 100;
 
